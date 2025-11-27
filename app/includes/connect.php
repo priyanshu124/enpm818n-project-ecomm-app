45e74c8bb795b4f74d2be2cd72abcee1826ddc7a
priyanshu124 <79084050+priyanshu124@users.noreply.github.com>
2025-11-27 04:05:58 -0500

    set db crednetials

--- app/includes/connect.php
+++ app/includes/connect.php
@@ -1,10 +1,5 @@
 <?php 
-$host = getenv('DB_HOST') ?: 'db';  // 'db' MySQL container name in docker-compose
-$user = getenv('DB_USER') ?: 'root';
-$pass = getenv('DB_PASSWORD') ?: '';
-$db   = getenv('DB_NAME') ?: 'ecommerce_1';
-
-$con = new mysqli($host, $user, $pass, $db);
+$con = new mysqli('localhost','root','','ecommerce_1');
 if(!$con){
     die(mysqli_error($con));
 }
