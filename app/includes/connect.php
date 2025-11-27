fa341f5067d42451a83eec60f20cf323ecd090b6
priyanshu124 <79084050+priyanshu124@users.noreply.github.com>
2025-11-27 04:03:29 -0500

    Convert app from submodule to normal folder

---
+++ app/includes/connect.php
@@ -0,0 +1,15 @@
+<?php 
+$host = getenv('DB_HOST') ?: 'db';  // 'db' MySQL container name in docker-compose
+$user = getenv('DB_USER') ?: 'root';
+$pass = getenv('DB_PASSWORD') ?: '';
+$db   = getenv('DB_NAME') ?: 'ecommerce_1';
+
+$con = new mysqli($host, $user, $pass, $db);
+if(!$con){
+    die(mysqli_error($con));
+}
+
+
+
+
+?>
