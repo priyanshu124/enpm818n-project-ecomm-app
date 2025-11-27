49efdbe20ba6f07f5ad1bdb2ddf661b5601e1483
priyanshu124 <79084050+priyanshu124@users.noreply.github.com>
2025-11-26 19:15:53 -0500

    Added main with backend setup and variables

---
+++ deploy/variables.tf
@@ -0,0 +1,21 @@
+variable "prefix" {
+  default = "enpm818n-"
+}
+
+variable "project" {
+  default = "ecoomerce-app"
+}
+
+variable "contact" {
+  default = "priyanshu12498@gmail.com"
+}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+
+variable "instance_type" {
+  description = "EC2 instance type for ECS container instances"
+  default     = "t2.micro"
+}
